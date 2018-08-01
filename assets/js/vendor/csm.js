d0b86d4de7598ee7beff9f4eec5b01268841c2a2
Jaisen Mathai <jaisen@jmathai.com>
2018-07-31 23:52:44 -0700

    Move js to file

---
+++ assets/js/vendor/csm.js
@@ -0,0 +1,27 @@
+  $(document).ready( function () {
+      // I only have one form on the page but you can be more specific if need be.
+      var $form = $('form.js');
+
+      if ( $form.length > 0 ) {
+          $form).bind('submit', function ( event ) {
+              if ( event ) event.preventDefault();
+              // validate_input() is a validation function I wrote, you'll have to substitute this with your own.
+              register($form);
+          });
+      }
+  });
+
+  function register($form) {
+      $.ajax({
+          type: $form.attr('method'),
+          url: $form.attr('action'),
+          data: $form.serialize(),
+          cache       : false,
+          dataType    : 'json',
+          contentType: "application/json; charset=utf-8",
+          error       : function(err) { $('.mce-success-response').hide(); $('.mce-error-response').show();  },
+          success     : function(data) {
+              $('.mce-error-response').hide(); $('.mce-success-response').show();
+          }
+      });
+  }
